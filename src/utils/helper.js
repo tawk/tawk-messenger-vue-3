bdb8236b3f4ed008b2ff2d5214822b1eb9f1d4de
Jerald Austero <jerald.austero@tawk.to>
2022-04-19 14:52:33 +0800

    Removed unused helpers

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,25 +6,6 @@ function isValidString(value) {
 	return value !== null && value !== undefined && typeof value === 'string';
 }
 
-function isValidFunction(func) {
-	if (!func) {
-		return false;
-	}
-
-	return func !== null && func !== undefined && typeof func === 'function';
-}
-
-function isEmittable(app, name) {
-	if (app._component &&
-		(app._component.emits && typeof app._component.emits === 'object')) {
-		return isValidFunction(app._component.emits[name]);
-	}
-
-	return false;
-}
-
 export {
-	isValidString,
-	isValidFunction,
-	isEmittable
+	isValidString
 };
